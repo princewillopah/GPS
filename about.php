be0b4bd223a353ef335bad1be79265c18c227865
Princewill Opah <princewillopah.dev@gmail.com>
2023-08-13 16:36:21 +0100

    from office

--- about.php
+++ about.php
@@ -75,136 +75,9 @@
 
 
 <!-- what we offer start -->
-     <div class="rts-offer-provide-section rts-section-gap our-service">
-        <div class="container">
-            <div class="row g-24">
-                <div class="col-12">
-                    <h4>Our Services</h4>
-                    <p>Championing the progress of the energy transition to create a sustainable future.</p>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#storage" class="thumbnail">
-                            <img src="assets/images/service/07.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#storage">
-                                <h5 class="title">
-                                Storage
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#storage" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#transportation" class="thumbnail">
-                            <img src="assets/images/service/08.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#transportation">
-                                <h5 class="title">
-                                Transportation
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#transportation" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#multimodal-logistics" class="thumbnail">
-                            <img src="assets/images/service/07.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#multimodal-logistics">
-                                <h5 class="title">
-                                Multimodal Logistics
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#multimodal-logistics" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#speciality-services" class="thumbnail">
-                            <img src="assets/images/service/08.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#speciality-services">
-                                <h5 class="title">
-                                Speciality Services
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#speciality-services" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#processing-and-refining" class="thumbnail">
-                            <img src="assets/images/service/09.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#processing-and-refining">
-                                <h5 class="title">
-                                Processing & Refining
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#processing-and-refining" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#blending-and-heating" class="thumbnail">
-                            <img src="assets/images/service/07.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#blending-and-heating">
-                                <h5 class="title">
-                                Blending & Heating
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#blending-and-heating" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-            </div>
-        </div>
-    </div>
+<?php
+	include "includes/our-service.php";
+?>
     <!-- what we offer end -->
 
 
@@ -279,7 +152,7 @@
 
 
   <!-- Working process start -->
-  <div class="rts-working-process-area rts-section-gap home-seven">
+     <div class="rts-working-process-area rts-section-gap home-seven">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
--- index.php
+++ index.php
@@ -172,7 +172,7 @@
                     <div class="about-one-left-wrapper   reveal-item overflow-hidden aos-init">
                         <div class="reveal-item overflow-hidden aos-init">
                             <div class="reveal-animation reveal-end reveal-primary aos aos-init" data-aos="reveal-end"></div>
-                            <img src="assets/images/about/01.jpg" alt="about-area">
+                            <img src="assets/images/_home/about.jpg" alt="about-area">
                             <div class="left-detail-thumb">
                                 <h5 class="title">
                                 Leading Provider of Strategic <br>
@@ -304,137 +304,10 @@
     </div>
     <!-- rts about area end -->
 
-     <!-- what we offer start -->
-     <div class="rts-offer-provide-section rts-section-gap our-service">
-        <div class="container">
-            <div class="row g-24">
-                <div class="col-12">
-                    <h4>Our Services</h4>
-                    <p>Championing the progress of the energy transition to create a sustainable future.</p>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#storage" class="thumbnail">
-                            <img src="assets/images/service/07.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#storage">
-                                <h5 class="title">
-                                Storage
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#storage" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#transportation" class="thumbnail">
-                            <img src="assets/images/service/08.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#transportation">
-                                <h5 class="title">
-                                Transportation
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#transportation" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#multimodal-logistics" class="thumbnail">
-                            <img src="assets/images/service/07.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#multimodal-logistics">
-                                <h5 class="title">
-                                Multimodal Logistics
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#multimodal-logistics" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#speciality-services" class="thumbnail">
-                            <img src="assets/images/service/08.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#speciality-services">
-                                <h5 class="title">
-                                Speciality Services
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#speciality-services" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#processing-and-refining" class="thumbnail">
-                            <img src="assets/images/service/09.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#processing-and-refining">
-                                <h5 class="title">
-                                Processing & Refining
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#processing-and-refining" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                    <!-- single offer area sart -->
-                    <div class="rts-single-offer">
-                        <a href="other-services.php#blending-and-heating" class="thumbnail">
-                            <img src="assets/images/service/07.jpg" alt="service">
-                        </a>
-                        <div class="content-wrapper">
-                            <a href="other-services.php#blending-and-heating">
-                                <h5 class="title">
-                                Blending & Heating
-                                </h5>
-                            </a>
-                            <!-- <p class="disc">
-                                Dui lobortis scelerisque magna curabitur duis purus platea massa accumsan
-                            </p> -->
-                            <a href="other-services.php#blending-and-heating" class="rts-btn btn-transparent-service">View Service<i class="fa-light fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- single offer area end -->
-                </div>
-            </div>
-        </div>
-    </div>
+<!-- what we offer start -->
+<?php
+	include "includes/our-service.php";
+?>
     <!-- what we offer end -->
 
 
